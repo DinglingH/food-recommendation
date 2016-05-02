dcd3bb54acf407dffd0a94f8477e6071899097ad
DinglingH <handingling@gmail.com>
2016-05-01 21:17:58 -0700

    Version 0.0.2: Use raccoon instead of likely

---
+++ README.md
@@ -0,0 +1,37 @@
+
+# Amazon Fine Food Recommendation
+
+## Data
+The data of Amazon fine food reviews is downloaded from Kaggle : https://www.kaggle.com/snap/amazon-fine-food-reviews
+
+## Purpose
+The purpose of this mini project includes:
+* Fetch data from Sqlite database.
+* Use Collaborative Filtering Algorithm to build recommendation for active users.
+* Store the recommendations in Elasticsearch.
+
+## Data Analysis Process
+### Raw Data
+Raw data contains **568454** reviews of **74258** food products by **256059** users.
+
+### Data Exploratory
+Considering calculation performance, only 'useful reviews' by 'active users' are used for data exploratory.
+Definition of 'useful review': review that has been marked as 'helpful'.
+Definition of 'active user': user who have wrote reviews for more than **20** products. The total number of 'active user' is **1703**.
+And we will only build recommendations for these active users.
+
+### Models & Algorithms
+Many libraries offer Collaborative Filtering Algorithm implementation. The library likely.js is tried first. And it uses matrix factorization. When the row or column gets more than 1000 elements, the program will run out of memory. So raccoon.js is implemented finally. It works fine for 10,000 of training reocords. Here are basic features of the algorithm:
+  * Jaccard Coefficient for Similarity
+  * K-Nearest Neighbors Algorithm for Recommendations
+  * Wilson Score Confidence Interval for a Bernoulli Parameter
+  * Redis  
+
+### Result
+Recommendations for all active users are saved in Elasticsearch.
+
+## Libraries Used
+The libraries used in the project include:
+* sqlite3: for reading from Sqlite database;
+* raccoon: for building recommendations with Collaborative Filtering Algorithm
+* elasticsearch: for writing to Elasticsearch database;
--- app.js
+++ app.js
@@ -1,80 +1,82 @@
 //Libraries
 var sqlite = require('sqlite3'),
-    Recommender = require('likely');
+    raccoon = require('raccoon'),
+    elasticsearch = require('elasticsearch');
 
-//Declare variables
-var inputMatrix = [];
-var labelCount = 0;
-var rowLabels = [];
-var userMap = {};
-var colLabels = [];
-var productMap = {};
+//Connect to Sqlite
 var db = new sqlite.Database('data/database.sqlite');
 
-//Retrieve data from database
-function getDataFromDb(sqlText, callback){
-  var dataResult = [];
-  db.each(sqlText, function (err, row) {
-    dataResult.push(row);
-  }, function (err, count){
-    callback(err, dataResult);
-  });
+//Connect to redis server, before running this program redis server should be started.
+raccoon.config.className = 'food';
+raccoon.connect(6379, '127.0.0.1');
+raccoon.flush();
+
+//Connect to elasticsearch, before running this program elasticsearch server should be started.
+var client = new elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'trace'
+});
+
+//declare variables
+var count = 0;
+var numOfTasks = 0;
+var countOfBuild = 0;
+var numOfTasksBuild = 0;
+
+function buildComplete(){
+  countOfBuild++;
+
+  if(countOfBuild === numOfTasksBuild){
+    process.exit(0);
+  }
 }
 
 function buildRecommendation(){
-  //Initialize input matrix, set all value as 0.
-  for(var r = 0; r < rowLabels.length; r++){
-    var sparseArray = [];
-    for(var c = 0; c < colLabels.length; c++){
-      sparseArray.push(0);
-    }
-    inputMatrix.push(sparseArray);
-  }
-  //update input matrix with scores
-  getDataFromDb('select UserId, ProductId, Score from training_reviews order by UserId, ProductId',
-    function (err, result) {
-      for( var a = 0; a < result.length; a++){
-        var user = result[a].UserId;
-        var product = result[a].ProductId;
-        var score = result[a].Score;
-        inputMatrix[userMap[user]][productMap[product]] = score;
+  numOfTasksBuild++;
+  console.log('before db each');
+  db.each('select distinct UserId from training_reviews', function (err, row) {
+    numOfTasksBuild++;
+    console.dir(row);
+    raccoon.recommendFor(row.UserId, 5, function (results) {
+      console.dir(results);
+      //create json object
+      var params = {
+        index: 'affindex',     //amazon fine food index
+        type: 'afftype',       //amazon fine food type
+        id: row.UserId,        //unique id
+        body: {
+          recommendations: results//recommendation
+        }
       }
-      console.log(inputMatrix.length, rowLabels.length, colLabels.length);
-
-      //build recommendaion model
-      var model = Recommender.buildModel(inputMatrix, rowLabels, colLabels);
-      var recommendations = model.recommendations(process.argv[2]);
-      for( var l = 0; l < recommendations.length; l++){
-        console.log(recommendations[l]);
-      }
+      //create elasticsearch object
+      numOfTasksBuild++;
+      client.create(params, function (err, response) {
+        buildComplete();
+      });
+      buildComplete();
     });
-
+  }, buildComplete
+  );
 }
 
-//set row labels
-getDataFromDb('select distinct UserId from training_reviews order by UserId', function (err, result) {
-  rowLabels = result.map(function (u) { return u.UserId; });
-  //Initialize user map
-  for(var i = 0; i < rowLabels.length; i++){
-    userMap[rowLabels[i]] = i;
-  }
-  //if row lables & column labels both completed, create input matrix
-  labelCount++;
-  if( labelCount === 2){
+function taskComplete(){
+  count++;
+  if(count === numOfTasks){
+    console.log('before calling Recommender');
     buildRecommendation();
   }
-});
+}
 
-//set column labels
-getDataFromDb('select distinct ProductId from training_reviews order by ProductId', function (err, result) {
-  colLabels = result.map(function (u) { return u.ProductId; });
-  //Initalize product map
-  for(var j = 0; j < colLabels.length; j++){
-    productMap[colLabels[j]] = j;
-  }
-  labelCount++;
-  //if row lables & column labels both completed, create input matrix
-  if( labelCount === 2){
-    buildRecommendation();
-  }
-});
+//Set liked training data for the model
+numOfTasks++;
+db.each('select UserId, ProductId from training_reviews where Score >= 3', function (err, row) {
+  numOfTasks++;
+  raccoon.liked(row.UserId, row.ProductId, taskComplete);
+}, taskComplete);
+
+//Set disliked training data for the model
+numOfTasks++;
+db.each('select UserId, ProductId from training_reviews where Score < 3', function (err, row) {
+  numOfTasks++;
+  raccoon.disliked(row.UserId, row.ProductId, taskComplete);
+}, taskComplete);
